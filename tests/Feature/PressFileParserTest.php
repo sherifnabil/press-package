4b04eab0c8f27e2fd6a86e11e4a8e91003598ad7
Sherif <sherifnabil@github.com>
2022-01-28 01:00:18 +0200

    markdown file test

---
+++ tests/Feature/PressFileParserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SherifNabil\Press\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+use SherifNabil\Press\PressFileParser;
+
+class PressFileParserTest extends TestCase
+{
+    /** @test */
+    public function the_head_and_body_gets_split()
+    {
+        $pressFileParser = (new PressFileParser(__DIR__ . '/../blogs/MarkFile1.md'));
+        $data = $pressFileParser->getData();
+
+        $this->assertStringContainsString('title: My Title', $data[1]);
+        $this->assertStringContainsString('description: Description here', $data[1]);
+        $this->assertStringContainsString('Blog post body', $data[2]);
+    }
+}
