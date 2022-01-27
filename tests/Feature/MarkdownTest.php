a5b072532f55372510af879ed088e5e56e337ad5
Sherif <sherifnabil@github.com>
2022-01-28 01:00:03 +0200

    testing markdown

---
+++ tests/Feature/MarkdownTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SherifNabil\Press\Tests\Feature;
+
+use Parsedown;
+use Orchestra\Testbench\TestCase;
+use SherifNabil\Press\MarkdownParser;
+
+class MarkdownTest extends TestCase
+{
+    /** @test */
+    public function simple_markdown_is_parsed()
+    {
+        $parsedown = MarkdownParser::parse('# Sherif');
+        $this->assertEquals($parsedown, "<h1>Sherif</h1>");
+    }
+}
