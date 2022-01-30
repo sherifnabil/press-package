831965c932db0995fad5131648d75bc7de9f4763
Sherif <sherifnabil@github.com>
2022-01-30 05:10:00 +0200

    dir not existing fix

--- src/Drivers/FileDriver.php
+++ src/Drivers/FileDriver.php
@@ -2,13 +2,19 @@
 
 namespace Sherif\Press\Drivers;
 
+use Sherif\Press\Drivers\Driver;
 use Sherif\Press\PressFileParser;
 use Illuminate\Support\Facades\File;
 
-class FileDriver
+class FileDriver extends Driver
 {
     public function fetchPosts()
     {
+        if (!is_dir(config('press.path'))) {
+            mkdir(config('press.path'));
+            file_put_contents(config('press.path') . '/test.md', file_get_contents(__DIR__ . '/../../blogs/sample-post.md'));
+        }
+
         $files = File::files(config('press.path'));
 
         foreach ($files as $file) {
