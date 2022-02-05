0a5be50ca95a471b9a8d136c11d3bfa5f36219de
Sherif <sherifnabil@github.com>
2022-02-05 04:05:08 +0200

    adding post extra fields gettter

--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -3,6 +3,7 @@
 namespace Sherif\Press\Repositories;
 
 use Sherif\Press\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class PostRepository
@@ -15,7 +16,14 @@ class PostRepository
             'slug'          =>  Str::slug($post['title']),
             'title'         =>  $post['title'],
             'body'          =>  $post['body'],
-            'extra'         =>  $post['extra'] ?? json_encode([]),
+            'extra'         =>  $this->extra($post),
         ]);
+    }
+
+    private function extra($post)
+    {
+        $extra = json_decode($post['extra'] ?? '[]', true);
+        $attributes = Arr::except($post, ['title', 'identifier', 'body', 'extra']);
+        return json_encode(array_merge($extra, $attributes));
     }
 }
